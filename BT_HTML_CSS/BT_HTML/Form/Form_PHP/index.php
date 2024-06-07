7d5e7207c445c319e2456a44eca690726b97c43e
Phu <tui.la.phu.6549@gmail.com>
2024-06-07 07:22:11 +0700

    update bai moi

---
+++ BT_HTML_CSS/BT_HTML/Form/Form_PHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tao Form Don Gian</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form name="Login" action="form_ac.php" method="get">
+        <label for="username"></label>
+        <input type="text" id="username" name="username"/><br>
+        <label for="password"></label>
+        <input type="text" id="password" name="password"/> <br>
+    </form>
+</body>
+</html>
